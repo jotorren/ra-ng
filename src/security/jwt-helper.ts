97e66a9494aa93998f77962d1b0bf3ac2125b71f
Jorge Torrente <jorge.torrente@t-systems.com>
2016-12-28 12:12:08 +0100

    tslint: Declaration of public instance member function not allowed to appear after declaration of private instance member function

--- src/security/jwt-helper.ts
+++ src/security/jwt-helper.ts
@@ -16,41 +16,6 @@ export class JwtHelper {
         return this.b64DecodeUnicode(output);
     }
 
-    // credits for decoder goes to https://github.com/atk
-    private b64decode(str: string): string {
-        let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-        let output = '';
-
-        str = String(str).replace(/=+$/, '');
-
-        if (str.length % 4 === 1) {
-            throw new Error('\'atob\' failed: The string to be decoded is not correctly encoded.');
-        }
-
-        for (
-            // initialize result and counters
-            let bc = 0, bs: any, buffer: any, idx = 0;
-            // get next character
-            buffer = str.charAt(idx++);
-            // character found in table? initialize bit storage and add its ascii value;
-            ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
-                // and if not first of each 4 characters,
-                // convert the first 8 bits to one ascii character
-                bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
-        ) {
-            // try to find character in table (0-63, not found => -1)
-            buffer = chars.indexOf(buffer);
-        }
-        return output;
-    }
-
-    // https://developer.mozilla.org/en/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_Unicode_Problem
-    private b64DecodeUnicode(str: any) {
-        return decodeURIComponent(Array.prototype.map.call(this.b64decode(str), (c: any) => {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-    }
-
     public decodeToken(token: string): any {
         let parts = token.split('.');
 
@@ -90,5 +55,40 @@ export class JwtHelper {
 
         // Token expired?
         return !(date.valueOf() > (new Date().valueOf() + (offsetSeconds * 1000)));
+    }
+
+    // credits for decoder goes to https://github.com/atk
+    private b64decode(str: string): string {
+        let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        let output = '';
+
+        str = String(str).replace(/=+$/, '');
+
+        if (str.length % 4 === 1) {
+            throw new Error('\'atob\' failed: The string to be decoded is not correctly encoded.');
+        }
+
+        for (
+            // initialize result and counters
+            let bc = 0, bs: any, buffer: any, idx = 0;
+            // get next character
+            buffer = str.charAt(idx++);
+            // character found in table? initialize bit storage and add its ascii value;
+            ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+                // and if not first of each 4 characters,
+                // convert the first 8 bits to one ascii character
+                bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+        ) {
+            // try to find character in table (0-63, not found => -1)
+            buffer = chars.indexOf(buffer);
+        }
+        return output;
+    }
+
+    // https://developer.mozilla.org/en/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_Unicode_Problem
+    private b64DecodeUnicode(str: any) {
+        return decodeURIComponent(Array.prototype.map.call(this.b64decode(str), (c: any) => {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
     }
 }
